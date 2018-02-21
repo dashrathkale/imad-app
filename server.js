e370bfc5c9da81c10ef0c31afd0106d8189cb797
dashrathkale <36568284+dashrathkale@users.noreply.github.com>
2018-02-21 22:47:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/projector Query',function(req,res){
+    res.sendfile('register your projector problem   ');
+})
+app.get('/projector Query1',function(req,res){
+    res.sendfile('register your projector problem   ');
+})
+app.get('/projector Query2',function(req,res){
+    res.sendfile('register your projector problem   ');
+})
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
