3d3e9900a13dcd9c0661289e70123d796ca45e9a
dashrathkale <36568284+dashrathkale@users.noreply.github.com>
2018-02-23 00:53:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,10 +94,11 @@ app.get('/', function (req, res) {
 app.get('/:projectorname',function(req,res){
 var    projectorname=req.params.projectorname
     res.send( createtemplate (projector[projectorname]));
-})
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
   app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
