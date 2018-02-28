38b4df54df4308e7b977283b602ba4f4d883e900
dashrathkale <36568284+dashrathkale@users.noreply.github.com>
2018-03-01 03:50:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,14 @@ var element=document.getElementById('mt');
 element.innerHTML='dashrath';
 // code for image changes
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft + 'px';
+}
 img.onclick=function()
 {
-    img.style.marginLeft='200px';
+    var interval=setInterval(moveRight,100);
+    //img.style.marginLeft='200px';
 };
