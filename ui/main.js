593cebd2f1c3a167330a48f9b7c72b397c8e5288
dashrathkale <36568284+dashrathkale@users.noreply.github.com>
2018-03-01 03:21:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ console.log('Loaded!');
 
 var element=document.getElementById('mt');
 element.innerHTML='dashrath';
+// code for image changes
+var img=document.getElementById('madi')
+madi.onclick=function()
+{
+    madi.style.marginleft='100px';
+};
