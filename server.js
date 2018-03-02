84fd512c25b11ea8b367021750908df958fd885b
dashrathkale <36568284+dashrathkale@users.noreply.github.com>
2018-03-02 23:52:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,12 @@ ${date}
 return htmltemplate;
 
 }
-
+var counter=0;
+app.get('/counter',function(req,res)
+{
+    counter=counter+1;
+    res.send(counter.tostring());
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
