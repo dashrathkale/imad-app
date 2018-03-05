b1b802791ba156ef2d6b39e9873ea5e3a117a14f
dashrathkale <36568284+dashrathkale@users.noreply.github.com>
2018-03-06 01:42:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,7 +41,7 @@ var nameInput=document.getElementById('name');
 var name= nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
-{
+{
     var name=['name1','name2','name3','name4'];
     var list=' ';
     for(var i=0;i<name.length;i++)
