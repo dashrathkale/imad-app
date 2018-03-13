0e228d0b939fe1ea7060d47de6937be2402c1cf9
dashrathkale <36568284+dashrathkale@users.noreply.github.com>
2018-03-14 02:12:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
-var pool=require('pg').pool;
+var Pool=require('pg').Pool;
 var config={
     user:'dashrathkale',
     database:'dashrathkale',
