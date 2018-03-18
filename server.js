b752f61cc23d6aea77ff3c1755c85bdfc5278680
dashrathkale <36568284+dashrathkale@users.noreply.github.com>
2018-03-18 16:23:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ pool.query('select * from projector' ,function(err, result) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-  app.get('/ui/main.js', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
