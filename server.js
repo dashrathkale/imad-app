1011689ff33d9b05104936644e79370bc137f867
dashrathkale <36568284+dashrathkale@users.noreply.github.com>
2018-03-20 02:21:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.post('/login', function(req,res) {
                 var dbString=result.rows[0].password;
                 var salt=dbString.split('$')[2];
                 var hashedPassword=hash(password,salt);
-                if(hashesPassword===dbString){
+                if(hashedPassword===dbString){
                     res.send('creditials are correct');
                     
                 }
