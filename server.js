4e480bf5af6f110d7e59b058fa71a265ad4cbff3
dashrathkale <36568284+dashrathkale@users.noreply.github.com>
2018-03-22 01:41:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/login', function(req,res) {
         else
         {
             if(result.rows.length===0){
-                req.send(403).send('invalid passward');
+                res.send(403).send('invalid passward');
             }
             else{
                 var dbString=result.rows[0].password;
