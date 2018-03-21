bcad85d0f343e78ea536ee84c47acdefc31811f3
dashrathkale <36568284+dashrathkale@users.noreply.github.com>
2018-03-22 01:05:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,7 +45,7 @@ submit.onclick=function()
     console.log(username);
     console.log(password);
     request.open('POST','http://dashrathkale.imad.hasura-app.io/login',true);
-    request.setRequestHeader('Content-Type','applciation/json');
+    request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username: username, password:password}));
 };
 /*
